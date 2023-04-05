beba418574aacde15d8d9d3a51d5489c3074a4dc
Błażej Kowol <kowol.blazej@gmail.com>
2023-04-05 23:26:20 +0200

    Commit after cleaning up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import { Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Container>
+        <h1>Hello World</h1>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/Components/Pages/Footer/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+
+    return (
+        <div>
+            <p className="text-center small fw-light text-muted m-5">Copyright © BlogApp 2023</p>
+        </div>
+    )
+}
+
+export default Footer;
