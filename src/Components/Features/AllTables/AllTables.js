0c9d1c482ae35c5a40a35d7d4ccab903da7a4011
Błażej Kowol <kowol.blazej@gmail.com>
2023-04-07 00:27:33 +0200

    add redux

--- src/Components/Features/AllTables/AllTables.js
+++ src/Components/Features/AllTables/AllTables.js
@@ -6,13 +6,13 @@ const AllTables = () => {
   return (
     <div className="container">
         <section className="row p-0 border-bottom">
-            <Col className="col-lg-1 d-flex align-items-center">
+            <Col className="col-lg-1 p-0 d-flex align-items-center">
                 <h3 className="text-center">Table</h3>
             </Col>
-            <Col className="col-lg-1 d-flex align-items-center">
+            <Col className="col-lg-1 p-0 d-flex align-items-center">
                 <p className="m-0">Status:</p>
             </Col>
-            <Link className="col d-flex my-2 justify-content-end text-decoration-none text-light" to={"/table"}>
+            <Link className="col d-flex my-2 p-0 justify-content-end text-decoration-none text-light" to={"/table"}>
                 <button type="submit" className="text-white border border-none bg-primary rounded py-2">
                     Show more
                 </button>
